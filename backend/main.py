98383696a3a6d42e820695a3aa3a7434d8939052
Rohan Bokaria <bokaria2@illinois.edu>
2025-03-19 01:22:05 -0400

    final

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,13 @@ from routes import upload, runs, data
 app = FastAPI()
 
 # Add CORS middleware
+origins = [
+    "http://localhost:3000", 
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
